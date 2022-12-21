ee00c2046b42cc1e205df8277985fdaaa1167e41
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-12-21 10:27:36 +0600

    added clicker

---
+++ src/components/Counter.tsx
@@ -0,0 +1,63 @@
+import { observer } from "mobx-react-lite";
+import { counter } from "../store/counter";
+import { useEffect } from "react";
+import { runInAction } from "mobx";
+
+const store = counter();
+
+type Props = {
+  initialCount?: number;
+};
+
+const Counter = observer((props: Props) => {
+  useEffect(() => {
+    runInAction(() => {
+      store.count = props.initialCount ?? 0;
+    });
+  }, [props.initialCount]);
+  return (
+    <div>
+      <h1 style={{ color: store.color }}>{store.count}</h1>
+      <div className="btnWrapper">
+        <button onClick={store.inc}>+</button>
+        <button onClick={store.dec}>-</button>
+      </div>
+    </div>
+  );
+});
+
+export default Counter;
+
+// import { observer, useLocalObservable } from "mobx-react-lite";
+
+// type Props = {
+//   initialCount?: number;
+// };
+
+// const Counter = observer((props: Props) => {
+//   const store = useLocalObservable(() => {
+//     return {
+//       count: props.initialCount ?? 0,
+//       get color() {
+//         return this.count > 0 ? "green" : this.count < 0 ? "red" : "black";
+//       },
+//       dec() {
+//         this.count--;
+//       },
+//       inc() {
+//         this.count++;
+//       },
+//     };
+//   });
+//   return (
+//     <div>
+//       <h1 style={{ color: store.color }}>{store.count}</h1>
+//       <div className="btnWrapper">
+//         <button onClick={store.inc}>+</button>
+//         <button onClick={store.dec}>-</button>
+//       </div>
+//     </div>
+//   );
+// });
+
+// export default Counter;
